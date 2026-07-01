a9ae715da8b7a6749505f399e960092f74de8602
asastelemedicina-cpu <asastelemedicina@gmail.com>
2026-07-01 15:15:48 -0300

    Update central-de-saude.tsx

--- src/routes/central-de-saude.tsx
+++ src/routes/central-de-saude.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { useState } from "react";
-import { Article } from "./central-de-saude.o-que-e-teleconsulta";
+//import { Article } from "./central-de-saude.o-que-e-teleconsulta";
 
 import {
   ArrowLeft,
