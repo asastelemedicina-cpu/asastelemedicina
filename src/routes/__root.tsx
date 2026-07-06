d3c4a88c9b3c459e4cd47e8701bec420c431ed11
asastelemedicina-cpu <asastelemedicina@gmail.com>
2026-07-06 02:27:53 -0300

    Update Google Tag Manager ID in __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -132,7 +132,7 @@ function RootShell({ children }: { children: ReactNode }) {
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;
               f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-W5NQ4BCF');
+              })(window,document,'script','dataLayer','GTM-W83VWG9F');
             `,
           }}
         />
