f5f523e0f571af8c5bf252cfda0837c0984e337d
asastelemedicina-cpu <asastelemedicina@gmail.com>
2026-07-15 09:38:20 -0300

    Add copyright notice to the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -101,7 +101,7 @@ export default function Footer() {
               <Instagram className="h-5 w-5 text-pink-500" />
               @asastelemedicina
             </a>
-
+             <p className="text-xs">© {new Date().getFullYear()} Todos os direitos reservados.</p>
           </div>
         </div>
 
