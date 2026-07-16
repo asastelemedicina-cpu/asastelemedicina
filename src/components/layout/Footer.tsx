b65a70339198dcc028555f810d6a8a0946ab7f64
asastelemedicina-cpu <asastelemedicina@gmail.com>
2026-07-15 23:24:06 -0300

    Update footer link to point to homepage

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
           <div>
             <div className="flex items-center gap-3">
               <a
-                href="/#top"       
+                href="/"       
                 className="flex items-center gap-3 transition hover:opacity-90"
               >
               <img
